b644281a7613487aab94c240834beeb50aa6506a
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2020-06-03 22:02:55 -0500

    added basic install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# custom installation for personal projects
+# uses all sqlboiler original features, just adds the view workaround, until I find the time to push a pr
+
+
+go install
+cd drivers/sqlboiler-psql
+go install
