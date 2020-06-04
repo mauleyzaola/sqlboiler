00b1f618c7fca750cdaa048dd858941185f6b37d
mauleyzaola <mguerrero@repay.com>
2020-06-04 09:33:23 -0500

    improved install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # custom installation for personal projects
 # uses all sqlboiler original features, just adds the view workaround, until I find the time to push a pr
 
-
+go get ./...
 go install
 cd drivers/sqlboiler-psql
 go install
